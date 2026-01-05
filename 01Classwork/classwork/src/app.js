8a628333685be4f0f29113a7e7089c27b2f171d2
HarshKumarhk07 <harshkumarhk525@gmail.com>
2026-01-05 16:39:07 +0530

    removing classwork

--- 01Classwork/classwork/src/app.js
+++
@@ -1,9 +0,0 @@
-let arr = [];
-let n = Number(prompt("Enter how many values you want:"));
-
-for (let i = 0; i < n; i++) {
-  let value = prompt("Enter value " + (i + 1));
-  arr.push(value);
-}
-
-console.log(arr);
